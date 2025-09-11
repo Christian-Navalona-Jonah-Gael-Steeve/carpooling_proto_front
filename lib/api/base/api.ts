3303eeb189048d8ad63b28af4909adde426fa613
Iharinavalona Rakotoniaina <iharinavalona.rakotoniaina@versusmind.eu>
2025-09-11 10:23:54 +0300

    add env files, complete TODO @api.ts, use API_BASE_URL from env vars

--- lib/api/base/api.ts
+++ lib/api/base/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// TODO: Replace with env variable
-export const API_BASE_URL = 'http://localhost:8080/api'
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
